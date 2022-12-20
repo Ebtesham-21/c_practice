f898f6829a40005ddfad9c1fbf78ca47688354ac
Ebtesham-21 <ebteshamhossain98@gmail.com>
2022-12-20 16:00:25 +0600

    c_practice

---
+++ sum_sequence/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a1, an, n;
+    printf("enter first num, last num and total: ");
+    //scanf can just take one data at a time
+    scanf("%d %d %d", &a1, &an, &n);
+
+    return 0;
+}
